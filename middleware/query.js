48c9c87e8514cf7f010647dace2e8924026f19d0
Jon Watkins <jonwatkins86@gmail.com>
2015-04-12 01:45:47 +0100

    changes

--- middleware/query.js
+++
@@ -1,9 +0,0 @@
-/**
- *  Middleware to sort out query string data
- *  @param  {Object} self
- *  @return {Function} init
- *  @api public
- */
-module.exports = function query (req, res, next) {
-  next();
-};
