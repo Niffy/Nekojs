9f1cf82ab7b92d6521be549272e0a26004e0e67c
Jon Watkins <jonwatkins86@gmail.com>
2015-04-12 01:00:21 +0100

    first commit

--- middleware/index.js
+++ middleware/index.js
@@ -2,4 +2,6 @@
  *  Export the core middleware functions
  */
 module.exports.init = require('./init');
-module.exports.query = require('./query');
+module.exports.query = require('nekojs-query');
+module.exports.favicon = require('nekojs-favicon');
+module.exports.static = require('nekojs-static');
